24f42b3e507bad195623ed568634993140bcfbb6
ckaufman <ckaufman@hsz-t.ch>
2011-03-12 01:53:58 +0100

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(:version => 20110301125021) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
+    t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
