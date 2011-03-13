12a22cbca692370bd6584fbf6a3ac86ad1722a8d
ckaufman <ckaufman@hsz-t.ch>
2011-03-13 20:40:36 +0100

    sync branches

--- db/migrate/20110301125021_create_products.rb
+++ db/migrate/20110301125021_create_products.rb
@@ -5,7 +5,6 @@ class CreateProducts < ActiveRecord::Migration
       t.text :description
       t.string :image_url
       t.decimal :price, :precision => 8, :scale => 2
-
       t.timestamps
     end
   end
