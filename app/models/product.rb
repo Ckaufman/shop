b83f0b013dca5818772176548ef2238816fef2d1
ckaufman <ckaufman@hsz-t.ch>
2011-04-10 00:45:24 +0200

    cart added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,22 @@
 class Product < ActiveRecord::Base
 	default_scope :order => 'title'
+	has_many :line_items
 	
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :title, :uniqueness => true
 	validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG or PNG image.'}
+	
+private
+	# ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		if line_items.count.zero?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end	
 end
