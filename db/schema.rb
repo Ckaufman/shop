4bc4aecd719820beeac26c60ffecff2699732104
ckaufman <ckaufman@hsz-t.ch>
2011-04-25 20:19:24 +0200

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110409213143) do
+ActiveRecord::Schema.define(:version => 20110409213830) do
 
   create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
