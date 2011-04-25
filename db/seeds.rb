c05146eba317674c7bb4c7d59538f5d80a329b00
ckaufman <ckaufman@hsz-t.ch>
2011-04-25 20:58:50 +0200

    product table filled again

---
+++ db/seeds.rb
@@ -0,0 +1,34 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
+# encoding: utf-8
+Product.delete_all
+
+Product.create(:title => 'Programming Ruby 1.9',
+  :description =>
+    %{<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>},
+  :image_url => '/images/ruby.jpg',
+  :price => 49.50)
+# . . .
+
+Product.create(:title => 'Rails Test Prescriptions',
+  :description => 
+    %{<p>
+        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
+        Rails applications, covering Test-Driven Development from both a
+        theoretical perspective (why to test) and from a practical perspective
+        (how to test effectively). It covers the core Rails testing tools and
+        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+      </p>},
+  :image_url => '/images/rtp.jpg',
+  :price => 43.75)
