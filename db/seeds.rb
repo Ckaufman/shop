ab997dabecc94327adaf67096f858e9173458b55
ckaufman <ckaufman@hsz-t.ch>
2011-05-08 15:13:48 +0200

    handling multiple cart items

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,21 @@
 # encoding: utf-8
 Product.delete_all
 
+Product.create(:title => 'Web Design for Developers',
+  :description => 
+    %{<p>
+        <em>Web Design for Developers</em> will show you how to make your
+        web-based application look professionally designed. We'll help you
+        learn how to pick the right colors and fonts, avoid costly interface
+        and accessibility mistakes -- your application will really come alive.
+        We'll also walk you through some common Photoshop and CSS techniques
+        and work through a web site redesign, taking a new design from concept
+        all the way to implementation.
+      <p>},
+  :image_url =>   '/images/wd4d.jpg',    
+  :price => 42.95)
+# . . .
+
 Product.create(:title => 'Programming Ruby 1.9',
   :description =>
     %{<p>
