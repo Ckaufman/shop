611cf713586c09acd38bf3ff21564b557289eab4
ckaufman <ckaufman@hsz-t.ch>
2011-05-08 15:41:38 +0200

    handling errors

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,12 +13,17 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.xml
   def show
-    @cart = Cart.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @cart }
-    end
+	begin
+		@cart = Cart.find(params[:id])
+	rescue ActiveRecord::RecordNotFound
+		logger.error "Attempt to access invalid cart #{params[:id]}"
+		redirect_to store_url, :notice => 'Invalid cart'
+	else
+		respond_to do |format|
+			format.html # show.html.erb
+			format.xml { render :xml => @cart }
+		end
+	end
   end
 
   # GET /carts/new
