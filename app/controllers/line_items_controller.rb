69560c60dbf5ee3596e0a66a89f17042a98bf5eb
ckaufman <ckaufman@hsz-t.ch>
2011-05-31 14:07:02 +0200

    moved cart to sidebar

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,7 +47,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart) }
+        #format.html { redirect_to(@line_item.cart) }
+		format.html { redirect_to(store_url) }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
